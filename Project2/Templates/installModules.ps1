a42a74f48d5157514e50093611e8af737e179abe
Vivek <45571449+vivekmishra1@users.noreply.github.com>
2019-04-15 11:47:36 +0530

    Delete installModules.ps1

--- Project2/Templates/installModules.ps1
+++
@@ -1,3 +0,0 @@
-﻿Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module xActiveDirectory, xNetworking, xStorage -Force
