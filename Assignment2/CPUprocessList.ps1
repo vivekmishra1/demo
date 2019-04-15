f96bad8988c25e9e3893594c4c9449b266091735
Vivek <45571449+vivekmishra1@users.noreply.github.com>
2019-04-15 11:29:10 +0530

    Update CPUprocessList.ps1

--- Assignment2/CPUprocessList.ps1
+++ Assignment2/CPUprocessList.ps1
@@ -1,5 +1,5 @@
 ﻿#Script to get top 10 CPU utilizing processes
-$Server = "IN-IT0891"
+$Server = "IN-IT1621"
 Get-Process -ComputerName $Server | Sort -Descending -Property CPU |Select-Object -First 10 `
 | Format-Table ProcessName, Id, SI, CPU
 
